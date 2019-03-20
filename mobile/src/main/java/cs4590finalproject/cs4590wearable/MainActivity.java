e132603b5ec4e4c955bb62be617d934bdeb2c2c3
sali45 <sali45@gatech.edu>
2019-03-20 19:21:33 -0400

    Processing dependency issue resolved.

--- mobile/src/main/java/cs4590finalproject/cs4590wearable/MainActivity.java
+++ mobile/src/main/java/cs4590finalproject/cs4590wearable/MainActivity.java
@@ -1,30 +1,43 @@
 package cs4590finalproject.cs4590wearable;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import processing.android.PFragment;
+import processing.android.CompatUtils;
+import processing.core.PApplet;
 
 public class MainActivity extends AppCompatActivity {
+    private PApplet sketch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CompatUtils.getUniqueViewId());
+        setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //here
-                startActivity(new Intent(MainActivity.this,CollectingData.class));
-            }
-        });
+        sketch = new Sketch();
+        PFragment fragment = new PFragment(sketch);
+        fragment.setView(frame, this);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (sketch != null) {
+            sketch.onRequestPermissionsResult(
+                    requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        if (sketch != null) {
+            sketch.onNewIntent(intent);
+        }
+    }
 }
--- mobile/src/main/java/cs4590finalproject/cs4590wearable/Sketch.java
+++ mobile/src/main/java/cs4590finalproject/cs4590wearable/Sketch.java
@@ -1,4 +1,17 @@
 package cs4590finalproject.cs4590wearable;
 
-public class Sketch {
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() { }
+
+    public void draw() {
+        if (mousePressed) {
+            ellipse(mouseX, mouseY, 50, 50);
+        }
+    }
 }
