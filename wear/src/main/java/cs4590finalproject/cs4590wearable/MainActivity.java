0b625f6906efb0d3b0311827d6bf4fa669f02a60
phillipwatson333 <pwatson30@gatech.edu>
2019-03-22 17:13:20 -0400

    im just trying to pull

---
+++ wear/src/main/java/cs4590finalproject/cs4590wearable/MainActivity.java
@@ -0,0 +1,14 @@
+package cs4590finalproject.cs4590wearable;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
